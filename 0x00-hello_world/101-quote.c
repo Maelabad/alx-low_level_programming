7d7d6860e3787d9e2a70ba8b8e0c1f8a5f61affb
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-02-16 22:53:40 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
 * main - Entry point
 * Return: Always return 1
@@ -12,6 +11,7 @@ int main(void)
 	char str[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
 	for (i = 0; i < 60 ; i++)
+		
 	{
 		putc(str[i], stdout);
 	}
