3fd5cd6caf429afa840c207c0adcfe0a7d648cad
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-02-16 23:04:45 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,8 @@
 int main(void)
 {
 	char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int i;
-	
-	for (i = 0; i < 60 ; i++)
+	int i;	
+	for (i = 0; i < 60 ; i++) 
 	{
 		putc(str[i], stdout);
 	}
