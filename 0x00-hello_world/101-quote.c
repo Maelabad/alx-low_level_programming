d077ecdb9cca0623c97994c565f080edbb5789d8
Maelabad <adjoboelie@gmail.com>
2023-02-16 14:29:10 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - Entry point
+* Return: Always return 1
+*/
+
+
+int main(void)
+{
+	str[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,"
+
+	for(int i=0; i<60 ; i++)
+	{
+
+		putc(str[i]);
+	}
+
+return (1);	
+
+}
