13baab90da95422a4679e268a5046cc327f146bd
Maelabad <adjoboelie@gmail.com>
2023-02-17 10:45:19 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Determine the nature of a number
+* Return: Always return 0
+*/
+
+int main(void)
+{
+short n;
+for (n = 0 ; n < 10 ; ch++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
