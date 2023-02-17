7d8fae7d2492d6539d8524078ead94e8cce1e958
Maelabad <adjoboelie@gmail.com>
2023-02-17 10:24:34 -0800

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Determine the nature of a number
+* Return: Always return 0
+*/
+
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+}
