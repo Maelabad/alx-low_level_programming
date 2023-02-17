9ccb07084dce20632c8fb97a385686e5423dfb6f
Maelabad <adjoboelie@gmail.com>
2023-02-17 10:08:04 -0800

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Determine the nature of a number
+* Return: Always return 0
+*/
+
+int main () {
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   }
+   putchar(\n);
+   
+   return(0);
+}
