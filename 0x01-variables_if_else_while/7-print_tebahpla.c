2777ff98ab36cc897cac47a128e87861641fefb5
Maelabad <adjoboelie@gmail.com>
2023-02-17 11:18:06 -0800

    REverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Determine the nature of a number
+* Return: Always return 0
+*/
+
+int main(void)
+{
+char ch;
+for (ch = 'z' ; ch >= 'a' ; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
