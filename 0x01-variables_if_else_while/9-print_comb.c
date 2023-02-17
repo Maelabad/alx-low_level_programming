8c311c733aa6f746bb533b008b7435e80797891b
Maelabad <adjoboelie@gmail.com>
2023-02-17 11:34:15 -0800

    Commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Print numbers in base 16
+* Return: Always return 0
+*/
+
+int main(void)
+{
+int n;
+for (n = 0 ; n < 10 ; n++)
+{
+putchar(n + '0');
+if ( n != 9 )
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+return (0);
+}
