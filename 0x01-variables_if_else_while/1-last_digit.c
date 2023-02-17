e46eb5e6aa7c088b085df01c266cacc89238a6cd
Maelabad <adjoboelie@gmail.com>
2023-02-17 09:46:32 -0800

    Last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Determine the nature of a number
+* Return: Always return 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	nb = n%10 ;
+	if (nb == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n , nb);
+	}
+	else if (n < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n , nb);
+	}
+	else if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n , nb);
+
+	}
+
+	return (0);
+}
