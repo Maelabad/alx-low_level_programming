062b7b9da0d8fa181174ee628f1565ce4d7ad603
Maelabad <adjoboelie@gmail.com>
2023-02-17 16:05:25 -0800

    Numbers combinaisons

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Print numbers in base 16
+* Return: Always return 0
+*/
+
+int main(void)
+{
+int n;
+int m;
+for (n = 0 ; n < 10 ; n++)
+{
+for (m = n + 1 ; m < 10 ; m++)
+{
+putchar(n + '0');
+putchar(m + '0');
+if (n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
