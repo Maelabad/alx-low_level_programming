0c048b16b0c4b0fc99860740b8c2d295c9a30baf
Maelabad <adjoboelie@gmail.com>
2023-02-17 16:54:00 -0800

    Combinaisons

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,11 @@ short a, b;
 short x, y;
 for (a = 0 ; a < 10 ; a++)
 {
-for (b = a + 1 ; b < 10 ; b++)
+for (b = 1 ; b < 10 ; b++)
 {
-for (x = b + 1 ; x < 10 ; x++)
+for (x = a ; x < 10 ; x++)
 {
-for (y = x + 1 ; y < 10 ; y++)
+for (y = b + 1 ; y < 10 ; y++)
 {
 putchar(a + '0');
 putchar(b + '0');
@@ -23,11 +23,13 @@ putchar(' ');
 putchar(x + '0');
 putchar(y + '0');
 
-if (a != 9 && b != 8)
+if (a == 9 && b == 8)
 {
+break;
+}
 putchar(',');
 putchar(' ');
-}
+
 }
 }
 }
