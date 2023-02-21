28472f58f64a99b0102b41216ac4d847148be05d
Maelabad <adjoboelie@gmail.com>
2023-02-21 14:36:07 -0800

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+
+#include"main.h"
+/**
+* print_last_digit - Last digit of a number
+* @n: variable representing the character
+* Return: Number between 0 to 9
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
