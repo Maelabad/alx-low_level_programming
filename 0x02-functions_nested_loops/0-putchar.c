a194d271fa5125d8f220b8c252589875809eac3f
Maelabad <adjoboelie@gmail.com>
2023-02-21 11:29:29 -0800

    -putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+* main - Print a String
+* Return: Always return 0
+*/
+
+int main(void)
+{
+	str = "_putchar"
+	int i=0;
+	while (i<strlen(str)
+	{
+		putchar(str[i];
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
