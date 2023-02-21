2c48f387e13681c7eb70fdee186d9cde38e8d010
Maelabad <adjoboelie@gmail.com>
2023-02-21 14:04:47 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+
+#include"main.h"
+/**
+* _isalpha - Check if a number is lower or upper character
+* @c: variable representing the character
+* Return: 1 if true , else 0
+*/
+
+int _isalpha(int c)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0')
+		return (0);
+	}
+	_putchar('-'
+	return (-1);
+}
+
