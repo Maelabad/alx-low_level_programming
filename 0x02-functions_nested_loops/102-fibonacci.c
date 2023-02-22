c6a745f325d4c529ea832a69908b94a110c89b60
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-02-22 20:03:47 +0000

    Fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ int main(void)
 int i = 3;
 int f1 = 1, f2 = 1, sum;
 
-	printf("%d, %d, ", f1, f2)
+	printf("%d, %d, ", f1, f2);
 	while (i < 50)
 	{
 		sum = f1 + f2;
-		printf("%d, ", sum)
+		printf("%d, ", sum);
 		f1 = f2;
 		f2 = sum;
 		i++;
