2c436ab3103b2dca1ecef5f7d43cca36d0a089e8
Maelabad <adjoboelie@gmail.com>
2023-02-22 12:19:26 -0800

    Many

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Display the 50th fibonacci numbers
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i = 3;
+unsigned long f1 = 1, f2 = 2, sum, sum_P;
+
+	printf("%ld, %ld, ", f1, f2);
+	while (sum < 4000000)
+	{
+		sum = f1 + f2;
+		if (sum % 2)
+		{
+			sum_P += sum;
+		}
+		printf("%ld, ", sum);
+		f1 = f2;
+		f2 = sum;
+		i++;
+	}
+	printf("%ld\n", sum_P);
+return (0);
+}
