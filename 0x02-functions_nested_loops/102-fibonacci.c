7f5774d7e9f98db1b810afe24d60636c6bf8c607
Maelabad <adjoboelie@gmail.com>
2023-02-22 12:02:08 -0800

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Display the 50th fibonacci numbers
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i = 3;
+int f1 = 1, f2 = 1, sum;
+
+	printf("%d, %d, ", f1, f2)
+	while (i < 50)
+	{
+		sum = f1 + f2;
+		printf("%d, ", sum)
+		f1 = f2;
+		f2 = sum;
+		i++;
+	}
+	printf("%d\n", fibo(i));
+return (0);
+}
