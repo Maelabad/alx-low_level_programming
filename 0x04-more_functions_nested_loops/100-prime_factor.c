4870d38397837545bd5ed69421f98ca26f80c41b
Maelabad <adjoboelie@gmail.com>
2023-02-23 15:13:08 -0800

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* main - print the largest prime factor
+* Return: 0
+*/
+
+int main(void)
+{
+	long long nb = 612852475143;
+	int largest_prime_factor = 2;
+
+	while (nb > largest_prime_factor)
+	{
+		if (nb % largest_prime_factor == 0)
+		{
+			nb = nb / largest_prime_factor;
+		}
+		else
+		{
+			largest_prime_factor++;
+		}
+	}
+
+	printf("%d\n", largest_prime_factor);
+	return (0);
+}
