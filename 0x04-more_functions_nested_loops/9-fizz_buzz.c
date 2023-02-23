5367f7611c095d04b02ae1e40e4581d396db7fe8
Maelabad <adjoboelie@gmail.com>
2023-02-23 11:42:03 -0800

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* main - print number 1 to 100
+* Return: Nothing
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			print("%d ", i);
+		}
+	}
+	printf("\n");
+}
