1d7fb2e820c8b7fe594e97271db477028b7336db
Maelabad <adjoboelie@gmail.com>
2023-02-23 09:20:57 -0800

    Is Digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - Check for a digits
+* @c: An integer 
+* Return: 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
