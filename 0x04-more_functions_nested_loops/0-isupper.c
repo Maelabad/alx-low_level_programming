189aa4e7bc1b1fad4b9b68ae7b6af4fec2bfe485
Maelabad <adjoboelie@gmail.com>
2023-02-23 09:10:33 -0800

    Upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - Check if a Character is upper
+* Return: 0
+*/
+
+int _isupper(int c)
+{
+	int i;
+
+	if (c >= 65 && c <= 91)
+	{
+		return (1);
+	}
+	return (0);
+}
+
