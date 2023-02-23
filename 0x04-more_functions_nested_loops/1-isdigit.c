a9e07c2c8e7f764cba116ca11f3b632e9198cc2e
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-02-23 20:26:48 +0000

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
 * _isdigit - Check for a digits
 * @c: An integer
-* Return: 0
+* Return: 0 or 1
 */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
