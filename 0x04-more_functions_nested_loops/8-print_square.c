5a8ef077f48c54f871f6508a1fc5b37f35dd12a7
Maelabad <adjoboelie@gmail.com>
2023-02-23 11:20:30 -0800

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_square - print a square
+* @size: An integer
+* Return: Nothing
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1 ; i <= size ; i++)
+	{
+		for (j = 1 ; j <= size ; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\');
+	}
+	_putchar('\n');
+}
