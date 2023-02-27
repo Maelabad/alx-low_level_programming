c4bafb8e8b6914a47776b9ca7d8a1190f3099dc5
Maelabad <adjoboelie@gmail.com>
2023-02-27 14:08:54 -0800

    N integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* puts_half - Display n elements of an array
+* @n: The numbers of elements to display
+* @a: A string to display
+* Return: Nothing
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(str[i]);
+		if (i < n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	_putchar('\n');
+}
