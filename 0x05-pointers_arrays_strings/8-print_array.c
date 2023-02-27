cb47c9cdabad2554198620d60a19bb50e18ffa34
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-02-27 22:35:46 +0000

    Array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,15 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i = 0, r = 0;
 
 	for (i = 0; i < n; i++)
 	{
+		do {
+			r = a[i] % 10;
+			_putchar(r + '\0');
+		} while (r > 9 );
+
 		_putchar(a[i]);
 		if (i < n - 1)
 		{
