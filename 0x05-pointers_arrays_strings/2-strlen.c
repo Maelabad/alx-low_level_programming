f7d65879776021e6ffa54cf8a39e14dee406faf5
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-02-27 18:56:50 +0000

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,13 @@
 /**
 * _strlen - Update the value of n to 98
 * @s: An pointer on an  integer
-* Return: A Size
+* Return: A number of int
 */
 
 int _strlen(char *s)
 {
-	return (sizeof(*s));
+	int i = 0;
+	while s[i] != '\0':
+		i++;
+	return (i);
 }
