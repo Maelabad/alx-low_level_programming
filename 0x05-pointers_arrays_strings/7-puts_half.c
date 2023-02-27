7f0ab606b891c58093020dd8da5eddd600da4d12
Maelabad <adjoboelie@gmail.com>
2023-02-27 13:58:07 -0800

    Second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* puts_half - Display a the 2nd half of a String
+* @str: A string to display
+* Return: Nothing
+*/
+
+void puts_half(char *str)
+{
+	int i = 0, count = 0, start = 0;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	if (count % 2 == 0)
+		start = count / 2;
+	else
+		start = (count - 1) / 2;
+
+
+	for (i = start; i <= count; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
