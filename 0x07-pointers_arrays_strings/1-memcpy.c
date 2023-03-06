440c72ca50a3b12024bade88166abb1e488faa9e
Maelabad <adjoboelie@gmail.com>
2023-03-06 14:29:35 -0800

    Memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* *_memcpy - fills memory with a constant byte.
+* @dest: Source buffer
+* @src: Destiantion buffer
+* @n: A number
+* Return: A number of char *
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
