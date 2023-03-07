7f20726b65545faf4dfb8d964f3a238a63568728
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-03-07 18:14:43 +0000

    Prototypes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,10 +14,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-
-
-
+void set_string(char **s, char *to);
 
 
 #endif /* MAIN_H */
