bf1973bc8392a36a26d5b6a07d4ca8440ed410b0
Maelabad <adjoboelie@gmail.com>
2023-03-08 12:37:06 -0800

    Prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input is prime number, otherwise 0.
+ * @n: A number
+ * Return: square root of a number.
+ */
+
+int is_prime_number(int n)
+{
+	int i = 2, mul = 0;
+
+	while (i < n)
+	{
+		if (n % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
