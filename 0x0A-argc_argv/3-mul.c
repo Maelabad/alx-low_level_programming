4fa41ee5f0cb543ab6af170eadbeed54cd064868
Maelabad <adjoboelie@gmail.com>
2023-03-10 15:44:07 -0800

    Multipplies

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+int main(int argc, char *argv[])
+{
+	int res = 0;
+
+	if (argc == 2)
+	{
+		res = argv[1] * argv[2];
+		print("%d\n", res);
+	}
+	else
+		printf("Error");
+
+	return (1);
+}
