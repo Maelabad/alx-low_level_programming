c0f3c4f234d53f692d0d98a7438847d7e994eff0
Maelabad <adjoboelie@gmail.com>
2023-03-13 17:17:40 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,19 @@
 char *_strdup(char *str)
 {
 	int i = 0, j;
+	char *array;
 
-	while (*str)
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
 		i++;
 
-	char *array = malloc(sizeof(char) * i);
+	array = (char *)malloc((sizeof(char) * i) + 1);
 
 	for (j = 0; j < i; j++)
 		array[j] = str[j];
+	array[i] = '\0';
+
 	return (array);
 }
