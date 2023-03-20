424654274cfa4fa1ce19325aa895c3e2e2724670
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-03-20 23:29:51 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,24 +13,13 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *dName, *dOwner;
-
-	dName = (char *)malloc(sizeof(char) * strlen(name));
-	dOwner = (char *)malloc(sizeof(char) * strlen(owner));
-	
-	if (dName == NULL || dOwner == NULL)
-		return NULL;
-
-	strcpy(dName, name);
-	strcpy(dOwner, owner);
-
 	dog_t *nDog = (dog_t *)malloc(sizeof(dog_t));
 
 	if (nDog == NULL)
 		return (NULL);
 
-	nDog->name = dName;
+	nDog->name = name;
 	nDog->age = age;
-	nDog->owner = dOwner;
+	nDog->owner = owner;
 	return (nDog);
 }
