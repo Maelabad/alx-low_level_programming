68885528c9224eea347456e38a0be5983a438978
Maelabad <adjoboelie@gmail.com>
2023-03-20 14:10:33 -0700

    A dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A Dog
+ * @name: The name
+ * @age:  The age
+ * @owner: The owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG.H*/
