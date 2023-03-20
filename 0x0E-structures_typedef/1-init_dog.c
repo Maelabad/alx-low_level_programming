765e0ed2981391dd64e1a10b952f4b0eeccfe8bc
Maelabad <adjoboelie@gmail.com>
2023-03-20 14:41:58 -0700

    Init Dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#inslude "dog.h"
+
+/**
+* init_dog - Initialise a dog
+* @d: a dog
+* @name: a name
+* @age: an age
+* @owner: an owner
+* Return: 0
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
