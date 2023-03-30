aee509ea12677cf5d2f92b9011aa033a4201c4e0
Maelabad <adjoboelie@gmail.com>
2023-03-30 11:40:13 -0700

    List

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_str - print a string
+ * @str: The string to display
+ * Return: Nothing
+ */
+void print_str(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str + '0');
+		str++;
+	}
+}
+
+/**
+* print_list - Print all the elements of a list
+* @h: List to display
+* Return: The number of elements
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			_putchar('[');
+			_putchar(('0');
+			_putchar(']');
+			_putchar(' ');
+			print_str("(nil)");
+		}
+		else
+		{
+			_putchar('[');
+			_putchar((*h).len + '0');
+			_putchar(']');
+			_putchar(' ');
+			print_str(h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i)
+}
